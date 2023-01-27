231f1f66996feb258cb747bc45082140ffa0a3e1
mic050r <s2216@e-mirim.hs.kr>
2023-01-28 02:05:09 +0900

    첫 번째 문제 중복제거 완

--- src/homework/MergeSort.java
+++ src/homework/MergeSort.java
@@ -12,11 +12,14 @@ import java.util.List;
 
 public class MergeSort {
 
-	public static void main(String[] args) {
+	public MergeSort() {
 		Scanner sc = new Scanner(System.in);
+
+		// 입력받을 데이터 선언
 		int a[] = new int[5]; // a 배열 선언
 		int b[] = new int[5]; // b 배열 선언
 		int sum[] = new int[a.length + b.length]; // a와b 값을 전부 넣을 sum 배열 선언
+
 		// 입력
 		System.out.print("첫 번쨰 배열의 데이터 : ");
 		for (int i = 0; i < a.length; i++) {
@@ -30,14 +33,34 @@ public class MergeSort {
 			sum[i + a.length] = b[i];
 		}
 
-		// 정렬
+		// 오름 차순 정렬
 		Arrays.sort(sum);
-		List<int[]> list = Arrays.asList(sum);
-		
-		
-		for (int i : sum) {
-			System.out.print(sum[i] + "  ");
+
+		// 중복 제거하기
+		int cnt = 0;
+		for (int i = 1; i < sum.length; i++) {
+			if (sum[i - 1] == sum[i]) {
+				cnt++;// 중복 개수 확인
+			}
 		}
+
+		int result[] = new int[sum.length - cnt];// 중복된 개수만큼 sum의 길이에서 빼기
+		int rIdx = 0; // result의 인덱스값
+		result[0] = sum[0]; // result[0]에 먼저 sum[0] 값을 넣어주기
+		for (int i = 1; i < sum.length; i++) {
+			if (result[rIdx] == sum[i])
+				continue; // 같다면
+			result[++rIdx] = sum[i];
+		}
+
+		System.out.print("병합된 배열의 데이터 : ");
+		for (int i = 0; i < result.length; i++) {
+			System.out.print(result[i] + " ");
+		}
+	}
+
+	public static void main(String[] args) {
+		new MergeSort();
 	}
 
 }
