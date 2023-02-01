97578457008c31ae07690248da53c9ba416c9e71
mic050r <s2216@e-mirim.hs.kr>
2023-02-01 20:21:18 +0900

    6번째 문제

---
+++ src/homework/Account.java
@@ -0,0 +1,20 @@
+package homework;
+
+/*
+#문제 6번 계좌를 위한 클래스를 정의하고, 계좌번호와 입금액
+과 출금액 입력받아 예시와 같이 잔액을 출력하는 프로
+그램을 작성
+<조건>
+조건 1. 클래스내에 다음 메서드를 작성하여 처리한다. 
+입금 메서드 : void deposit([매개변수]) 
+출금 메서드 : void withdraw([매개변수]) 
+출력 메서드(입출금 후 계좌번호 ***의 현재 잔액 : &&&& 메시지 출력하는 메서드) 이용
+ */
+public class Account {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
