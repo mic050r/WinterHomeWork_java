d710773806dbfe1dd20a98a530324514bfdd3e51
mic050r <s2216@e-mirim.hs.kr>
2023-02-05 18:30:20 +0900

    8번째 문제 년도별로 분기별/연간/누적 판매량 출력

--- src/homework/SalesVol.java
+++ src/homework/SalesVol.java
@@ -10,11 +10,12 @@ import java.util.Scanner;
 public class SalesVol {
 	public SalesVol() {
 		Scanner sc = new Scanner(System.in);
-		int sales[][] = new int[3][4];
+		int sales[][] = new int[3][4]; // 판매량
 		int annual = 0; // 연간 판매량
 		int cumulative = 0; // 누적 판매량
+		String str[] = { "\t", "  1분기 ", " 2분기 ", " 3분기 ", " 4분기 ", " 연간판매량 ", " 누적판매량 " };
 
-		String str[] = { "       ", "  1분기 ", " 2분기 ", " 3분기 ", " 4분기 ", " 연간판매량 ", " 누적 판매량 " };
+		// 분기별 판매량 입력
 		for (int i = 0; i < sales.length; i++) {
 			System.out.print((2019 + i) + "년도 : ");
 			for (int j = 0; j < sales.length + 1; j++) {
@@ -26,6 +27,17 @@ public class SalesVol {
 		}
 		System.out.println("");
 
+		// 년도별, 분기별 판매량과 연간판매량 누적판매량을 출력
+		for (int i = 0; i < sales.length; i++) {
+			System.out.print((2019 + i) + "년도 : "); // 년도 출력
+			for (int j = 0; j < sales.length + 1; j++) {
+				System.out.print(sales[i][j] + "   ");
+				annual += sales[i][j];
+			}
+			cumulative += annual;
+			System.out.print(" " + annual + "     ");
+			System.out.println(cumulative);
+		}
 	}
 
 	public static void main(String[] args) {
